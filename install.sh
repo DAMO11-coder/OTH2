f8ef11d83e81566d845d4be0a2a5819fdfc592ba
DAMO <77869423+DAMO11-coder@users.noreply.github.com>
2023-04-19 19:55:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ installTheme(){
     echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r IceMinecraftTheme
-    git clone https://github.com/Angelillo15/IceMinecraftTheme.git
+    git clone https://github.com/DAMO11-coder/OTH2.git
     cd IceMinecraftTheme
     rm /var/www/pterodactyl/resources/scripts/IceMinecraftTheme.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
@@ -52,7 +52,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/Angelillo15/IceMinecraftTheme/main/repair.sh)
+    bash <(curl https://github.com/DAMO11-coder/OTH2/main/repair.sh)
 }
 
 restoreBackUp(){
